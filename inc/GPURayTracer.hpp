0d21bae253181d0134eb45eeeb30e0d75f20e247
evelynm <evelynmann@comcast.net>
2013-11-05 01:04:08 -0500

    Forgot GPUManager.cpp adding it to the list of things

---
+++ inc/GPURayTracer.hpp
@@ -0,0 +1,101 @@
+/*
+ * Includes items necessary to construct an image or scene.
+ * Note prints functions could be set up to work as << operators
+ * but don't remember how to do that right now.
+ */
+
+#ifndef GPU_RAY_TRACER_H
+#define GPU_RAY_TRACER_H
+
+#include "RayTraceImageData.h"
+#include "jsonParameterReader.h"
+
+
+namespace grt {
+
+    class Model;
+
+    class IModelObserver {
+        public:
+            virtual void modelUpdated(const Model &) = 0;
+    };
+
+    class Model {
+        std::set<IModelObserver*> observers;
+
+        public:
+			RayTraceImageData image;
+
+            void registerObserver(IModelObserver * _obs) {
+                observers.insert(_obs);
+            }
+
+            void unregisterObserver(IModelObserver * _obs) {
+                observers.erase(_obs);
+            }
+
+            void notifyObservers() {
+				std::cout<<"Inside a Model class notifyObservers function"<<std::endl;
+                for (std::set<IModelObserver*>::iterator ob_it = observers.begin(); ob_it != observers.end(); ++ob_it) {
+					std::cout<<"Notifing an observer!"<<std::endl;
+                    (*ob_it)->modelUpdated(*this);
+                }
+            }
+    };
+
+    class ImageSaverView
+    : public IModelObserver
+    {
+        private:
+            std::string filename;
+        public:
+            ImageSaverView(const std::string & _filename)
+            : filename(_filename)
+            {}
+
+            void modelUpdated(const Model & _model) {
+                // TODO : save to file
+                std::cout << "Model Update received! [" << __FILE__ << ":" << __LINE__ << "]" << std::endl;
+            }
+    };
+
+    class Controller {
+        protected:
+            Model & model;
+        public:
+            Controller(Model & _model)
+            : model(_model)
+            {}
+
+            void execute() {}
+
+            // NOT IMPLEMENTED YET
+            void revert() {}
+    };
+
+    class ParseJSONController
+    : public Controller
+    {
+        protected:
+            boost::property_tree::ptree jsonPtree;
+        public:
+            ParseJSONController(Model & _model, const std::string & _jsonFile)
+            : Controller(_model)
+            {
+                boost::property_tree::read_json(_jsonFile,jsonPtree);
+				LoadRayTraceImageFromJsonFile(_jsonFile, model.image);
+            }
+
+            void execute() {
+ 
+                // let everyone know
+				std::cout<<"ParseJSONController calling model.notifyObservers"<<std::endl;
+                model.notifyObservers();
+            }
+    };
+
+};
+  
+
+#endif
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,93 +2,16 @@
 #include <iostream>
 #include <set>
 
-#include <boost/program_options.hpp>
+//#include <boost/program_options.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
 #include <Scene.hpp>
 #include <Image.hpp>
 #include <GPUManager.hpp>
+#include <GPURayTracer.hpp>
 
-namespace grt {
 
-    class Model;
-
-    class IModelObserver {
-        public:
-            virtual void modelUpdated(const Model &) = 0;
-    };
-
-    class Model {
-        std::set<IModelObserver*> observers;
-
-        public:
-            void registerObserver(IModelObserver * _obs) {
-                observers.insert(_obs);
-            }
-
-            void unregisterObserver(IModelObserver * _obs) {
-                observers.erase(_obs);
-            }
-
-            void notifyObservers() {
-                for (std::set<IModelObserver*>::iterator ob_it = observers.begin(); ob_it != observers.end(); ++ob_it) {
-                    (*ob_it)->modelUpdated(*this);
-                }
-            }
-    };
-
-    class ImageSaverView
-    : public IModelObserver
-    {
-        private:
-            std::string filename;
-        public:
-            ImageSaverView(const std::string & _filename)
-            : filename(_filename)
-            {}
-
-            void modelUpdated(const Model & _model) {
-                // TODO : save to file
-                std::cout << "Model Update received! [" << __FILE__ << ":" << __LINE__ << "]" << std::endl;
-            }
-    };
-
-    class Controller {
-        protected:
-            Model & model;
-        public:
-            Controller(Model & _model)
-            : model(_model)
-            {}
-
-            void execute() {}
-
-            // NOT IMPLEMENTED YET
-            void revert() {}
-    };
-
-    class ParseJSONController
-    : public Controller
-    {
-        protected:
-            boost::property_tree::ptree jsonPtree;
-        public:
-            ParseJSONController(Model & _model, const std::string & _jsonFile)
-            : Controller(_model)
-            {
-                boost::property_tree::read_json(_jsonFile,jsonPtree);
-            }
-
-            void execute() {
-                // TODO : Convert JSON PTree to Model
-
-                // Finished changing model, let everyone know
-                model.notifyObservers();
-            }
-    };
-
-};
 
 int main(int argc, char ** argv)
 {
@@ -96,7 +19,7 @@ int main(int argc, char ** argv)
     std::string img_file;
 
     // Setup program options
-    boost::program_options::options_description desc("Allowed options");
+    /*boost::program_options::options_description desc("Allowed options");
     desc.add_options()
         ("help,h","display this text")
         ("jsonfile,j",boost::program_options::value<std::string>(&json_file)->required(),"path to the json file describing the scene to render")
@@ -118,6 +41,23 @@ int main(int argc, char ** argv)
         std::cerr << "Program Options Parsing Error: " << e.what() << std::endl;
         return -1;
     }
+	*/
+
+	if (argc<2)
+	{
+		std::cout<<"Usage: "<<argv[0]<<" json_file img_file"<<std::endl;
+		std::cout<<"Where json_file is the path to the input json parameter file that describing the scene (required)"<<std::endl;
+		std::cout<<"And img_file is the path to save the rendered png file (optional)"<<std::endl;
+	}
+	else 
+	{
+		json_file = argv[1];
+	}
+	if (argc>2)
+	{
+		img_file = argv[2];
+	}
+	std::cout<<"Got command line arguments json_file = "<<json_file<<", img_file = "<<img_file<<std::endl;
 
     // Initialize GPU
     grt::GPUManager gman;
